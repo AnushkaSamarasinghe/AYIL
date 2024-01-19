f12037caab2e15d76a67166a35686061b25fbd2c
Anushka Samarasinghe <anushkapws@gmail.com>
2024-01-19 19:39:44 +0530

    modified package file

--- Package.swift
+++ Package.swift
@@ -16,8 +16,5 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "AYIL"),
-        .testTarget(
-            name: "AYILTests",
-            dependencies: ["AYIL"]),
     ]
 )
